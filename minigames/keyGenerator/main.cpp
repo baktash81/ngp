7250532f2bf3e375e3ba20890da2550fc751f4fa
ahmadreza tahmasbi <75021820+ahmadrezatmb@users.noreply.github.com>
2021-05-17 00:58:08 +0430

    Update main.cpp

---
+++ minigames/keyGenerator/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+#include <cstdlib>
+#include <ctime>
+#include <cstdlib>
+#include <unistd.h>
+// mini game to poison the AI
+using namespace std;
+class keyGenerator{
+	void gamerunner(){
+		//assemblening the other functions
+	}
+	void table_drawer(){
+		//output the table and its cells
+	}
+
+	bool validator(){
+		//validate the input string
+	}
+};
+
+int main(){
+	
+}
