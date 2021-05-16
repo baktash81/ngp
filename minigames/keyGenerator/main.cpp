11e32d01c8b2705a99906e3d37f5d58d2b8554c6
ahmadreza tahmasbi <75021820+ahmadrezatmb@users.noreply.github.com>
2021-05-17 01:03:09 +0430

    Update main.cpp

--- minigames/keyGenerator/main.cpp
+++ minigames/keyGenerator/main.cpp
@@ -6,6 +6,16 @@
 // mini game to poison the AI
 using namespace std;
 class keyGenerator{
+	int success = 0 ;
+	int fail = 0 ;
+	int first_i ;
+	int first_j ;
+	int second_i ;
+	int second_j ;
+	int len ;
+	void initialize(){
+		//initializing each data member each time
+	}
 	void gamerunner(){
 		//assemblening the other functions
 	}
