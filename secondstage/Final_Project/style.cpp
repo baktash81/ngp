669d000291b0eeb110851e1d1b270c7b628abd39
ahmadreza tahmasbi <75021820+ahmadrezatmb@users.noreply.github.com>
2021-05-24 23:47:09 +0430

    Create style.cpp

---
+++ secondstage/Final_Project/style.cpp
@@ -0,0 +1,10 @@
+#include <windows.h>
+#include "style.h"
+
+style::style(){
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+}
+
+void style::operator+ (int input){
+	SetConsoleTextAttribute(hConsole, input);
+}
