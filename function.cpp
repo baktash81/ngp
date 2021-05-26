e4c99bc621da40c667bdbf52ad468cf91d901b01
Baktash <baktash.ansari1381@gmail.com>
2021-05-26 19:24:55 +0430

    nesseccary functions

---
+++ function.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int tonum(string str){
+	// change the string format of number to integer format
+	char * arr;
+	int x=0;
+	arr = new char [str.length()];
+	for(int i=0;i<str.length();i++)
+		arr[i] = str[i];
+	for(int i=0;i<str.length();i++)
+		x = x*10 + (arr[i]-'0');
+	delete [] arr;
+	return x;
+}
+
+
+bool isDigit(string s){
+	// check the string is number or not
+	for(int i=0;i<s.size();i++){
+		if(isdigit(s[i])==false)
+			return false;
+	}
+	return true;
+}
+
+
+
+
