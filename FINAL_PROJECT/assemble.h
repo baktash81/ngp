71784962ed14aa0842794627544a2a01a69afcad
ahmadreza tahmasbi <75021820+ahmadrezatmb@users.noreply.github.com>
2021-05-26 23:49:26 +0430

    Create assemble.h

---
+++ FINAL_PROJECT/assemble.h
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+#include "key.h"
+#include "security_holes.h"
+#include "password.h"
+#ifndef REZ_ASSEMBLE_GAMES
+#define REZ_ASSEMBLE_GAMES
+class assemble{
+	public:
+	bool pass;
+	bool rezpass;
+	bool bakpass;
+	bool hesampass;
+	
+	//final stage:
+	bool firststagepass;
+	
+	keyGenerator lms;
+	Security_Holes second;
+	GameThree goverment;
+	
+	GameThree AIoff;
+	keyGenerator final ;
+	
+	assemble();
+	void gamelogic();
+	bool rez_gamerun(int);
+	void rez_game_resetloose();
+	bool baktash_gamerun(int);
+	bool hesam_gamerun();
+};
+#endif
