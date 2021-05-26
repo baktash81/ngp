3bcf4a77505060e128414fe11302581fcf87ea40
ahmadreza tahmasbi <75021820+ahmadrezatmb@users.noreply.github.com>
2021-05-26 23:49:39 +0430

    Create map.h

---
+++ FINAL_PROJECT/map.h
@@ -0,0 +1,20 @@
+#ifndef REFLEXES_MAP_H
+#define REFLEXES_MAP_H
+#include "member.h"
+#include <string>
+using namespace std;
+class Map{
+private :
+    int level;
+    Member m;Random r;
+    char array[20][20];
+    char width[20] = {'A','B','C','D','E','F','G','H','I','J',
+                            'K','L','M','N','O','P','Q','R','S','T'};
+public:
+    Map(int level);
+    string get_position();
+    string get_str(int index);
+    void print() const;
+};
+
+#endif //REFLEXES_MAP_H
